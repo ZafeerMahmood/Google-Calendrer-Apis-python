c3978365eb18067d2ce4551f83c9712073ab05d0
Zafeer Mahmood <zafeer7460@gmail.com>
2023-08-20 19:32:24 +0500

    poseclass working properly with calendar events

--- pose.py
+++ pose.py
@@ -3,30 +3,31 @@ This module is used to detect a pose and create a Google Calendar event if the u
 more than 10 seconds(changeable)."""
 import cv2
 import mediapipe as mp
-from imutils.video import VideoStream
-from google_calendar import create_event,get_credentials
+from google_calendar import create_event, get_credentials
 from googleapiclient.errors import HttpError
 from datetime import datetime, timedelta
 
-
 class PoseDetector:
-
     def __init__(self, url):
         self.url = url
-        self.vs = None
-        self.secs=10 # change this to change the time threshold eg 300 for 5 minutes
+        self.secs = 10  # change this to change the time threshold eg 300 for 5 minutes
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_pose = mp.solutions.pose
+        self.last_detection_time = None
+        self.black_screen_start_time = None
 
-    def detect_pose(self):
-        last_detection_time = None
-        pose_detected = False
-        
+    def detect_pose(self, video_path):
+        cap = cv2.VideoCapture(video_path)
+
+        if not cap.isOpened():
+            print("Error opening video")
+            return
+
         with self.mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
             while True:
-                frame = self.vs.read()
-                if frame is None:
-                    continue
+                ret, frame = cap.read()
+                if not ret:
+                    break
 
                 image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 image.flags.writeable = False
@@ -45,32 +46,50 @@ class PoseDetector:
                 cv2.imshow('Mediapipe Feed', image)
 
                 # Check if a pose is detected
-                if any(landmark.visibility > 0.5 for landmark in results.pose_landmarks.landmark):
-                    pose_detected = True
-                    if last_detection_time is None:
-                        last_detection_time = datetime.now()
+                if results.pose_landmarks and any(landmark.visibility > 0.5 for landmark in results.pose_landmarks.landmark):
+                    self.pose_detected_actions()
                 else:
-                    pose_detected = False
-                    if last_detection_time is not None:
-                        time_difference = datetime.now() - last_detection_time
-                        if time_difference.total_seconds() >= self.secs:  # Adjust the time threshold as needed
-                            try:
-                                create_event("Work Time", "Home", "Work session", last_detection_time.isoformat(),
-                                             (last_detection_time + timedelta(seconds=10)).isoformat(),
-                                             [], [], {'useDefault': False, 'overrides': [{'method': 'popup', 'minutes': 10}]})
-                            except HttpError as error:
-                                print('An error occurred:', error)
-                            last_detection_time = None
+                    self.pose_not_detected_actions()
 
                 if cv2.waitKey(10) & 0xFF == ord('q'):
                     break
 
+        cap.release()
         cv2.destroyAllWindows()
-        self.vs.stop()
+
+    def pose_detected_actions(self):
+        if self.last_detection_time is None:
+            self.last_detection_time = datetime.now()
+            self.black_screen_start_time = None  # Reset black screen timer
+
+    def pose_not_detected_actions(self):
+        if self.last_detection_time is not None:
+            time_difference = datetime.now() - self.last_detection_time
+            if time_difference.total_seconds() >= self.secs:  # Adjust the time threshold as needed
+                self.handle_black_screen()
+
+    def handle_black_screen(self):
+        if self.black_screen_start_time is None:
+            self.black_screen_start_time = datetime.now()
+        else:
+            black_screen_duration = datetime.now() - self.black_screen_start_time
+            if black_screen_duration.total_seconds() >= self.secs:
+                try:
+                    start_time = self.last_detection_time.isoformat()
+                    end_time = (self.last_detection_time + timedelta(seconds=10)).isoformat()
+                    event_link = create_event("Work Time", "Home", "Work session", start_time, end_time,
+                                              [], [], {'useDefault': False, 'overrides': [{'method': 'popup', 'minutes': 10}]})
+                    print('Event created:', event_link)
+                except HttpError as error:
+                    print('An error occurred:', error)
+                self.last_detection_time = None
+                self.black_screen_start_time = None
 
 # Example usage
 if __name__ == '__main__':
-    url = 'b.mp4'
     get_credentials()
-    pose_detector = PoseDetector(url)
-    pose_detector.detect_pose()
+    get_credentials()
+    video_path = 'b.mp4'  # Replace with the actual path to your video file
+    pose_detector = PoseDetector(video_path)
+    pose_detector.detect_pose(video_path)
+
